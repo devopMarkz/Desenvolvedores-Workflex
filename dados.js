2eb4cb3a9f2813ee73a66e123266fb13c0534931
devopMarkz <marcosdev2002@gmail.com>
2024-09-08 00:33:55 -0300

    First commit

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Marcos André",
+        descricao: "Marcos é um profissional fundamental no projeto WorkFlex, destacando-se especialmente no backend e no desenvolvimento de banco de dados. Sua atuação garantiu a robustez e eficiência das operações do sistema. Além disso, Marcos desempenha um papel crucial na colaboração com clientes, assegurando que suas necessidades e expectativas sejam bem compreendidas e atendidas, promovendo uma comunicação clara e produtiva.",
+        link: "https://github.com/devopMarkz",
+        tags: "dev marcos java"
+    },
+    {
+        titulo: "Paulo Monteiro",
+        descricao: "Paulo é um desenvolvedor front-end talentoso, responsável por criar interfaces digitais envolventes e funcionais no projeto WorkFlex. Sua expertise em HTML, CSS e JavaScript assegura que as páginas sejam visualmente atraentes e altamente responsivas. Além disso, Paulo trabalha de forma colaborativa com a equipe para garantir uma integração fluida entre o design e a funcionalidade, proporcionando uma experiência de usuário excepcional.",
+        link: "https://github.com/paulomonteiro3",
+        tags: "front dev paulo"
+    },
+    {
+        titulo: "Vitor Nascimento",
+        descricao: "Vitor Nascimento é um especialista em Python e análise de dados, responsável pelo desenvolvimento do banco de dados do projeto WorkFlex. Sua experiência e habilidades garantem a eficiência e integridade das informações no projeto.",
+        link: "#",
+        tags: "bd sql dados"
+    },
+    {
+        titulo: "Felippe Saraiva",
+        descricao: "Felippe Saraiva é um desenvolvedor habilidoso que, junto com Vitor Nascimento, desempenhou um papel crucial na modelagem do banco de dados do projeto WorkFlex. Sua experiência em design de banco de dados e sua capacidade analítica garantiram uma estrutura eficiente e escalável para o projeto. Felippe também contribui ativamente para a integração de sistemas e a otimização de processos, ajudando a garantir que a base de dados suporte de forma robusta as necessidades do WorkFlex.",
+        link: "#",
+        tags: "bd sql dados"
+    },
+    {
+        titulo: "João Felipe",
+        descricao: "João Felipe é um especialista essencial no projeto WorkFlex, colaborando com Vitor Nascimento e Felippe Saraiva na modelagem do banco de dados. Sua experiência e habilidade em design de banco de dados foram fundamentais para criar uma estrutura eficiente e escalável. João Felipe contribui significativamente para a otimização e integração do sistema, assegurando que a base de dados suporte de forma robusta as necessidades do projeto e melhore a performance geral do WorkFlex.",
+        link: "https://github.com/fecalu",
+        tags: "bd sql dados"
+    }
+];
